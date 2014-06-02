94dabe67910334c2fdf8ed98c95e6f5ad9013127
CCrosbyweb <Celeste@eagles.ewu.edu>
2014-06-02 14:07:39 -0700

    Version 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  $('form').validetta({
+  	realTime : true
+  });
+
+
+
+  // Your Scripts
+
+});
